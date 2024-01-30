2ecd7b28ba1b467d72794781427f670ec1426039
CodeLou <tamargo.louiel@gmail.com>
2024-01-30 13:35:29 +0800

    update copyright icon

--- app/ui/dashboard/footer/footer.jsx
+++ app/ui/dashboard/footer/footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className={styles.container}>
       <div className={styles.logo}>JLT Coder Dev</div>
-      <div className={styles.text}>C All rights reserved.</div>
+      <div className={styles.text}>&#169; All rights reserved.</div>
     </div>
   );
 };
