b0ea89b166870afa4cdd6af726221234fe9eec6b
CodeLou <tamargo.louiel@gmail.com>
2024-02-13 17:00:46 +0800

    next auth protected route, auth js

--- app/middleware.js
+++ app/middleware.js
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth";
-import { authConfig } from "./authconfig";
+import { authConfig } from "./app/authconfig";
 
 export default NextAuth(authConfig).auth;
 
 export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|.png.*)'],
-}
+    matcher: ['/((?!api|_next/static|_next/image|.png).*)'],
+};
