1c56cec076d63efb6bc067fe0939a25d3d1fd2c4
CodeLou <tamargo.louiel@gmail.com>
2024-02-13 16:28:05 +0800

    fetch product in product details page, update product in product details page

--- app/dashboard/products/[id]/page.jsx
+++ app/dashboard/products/[id]/page.jsx
@@ -1,27 +1,33 @@
+import { updateProduct } from "@/app/lib/actions";
+import { fetchProduct } from "@/app/lib/data";
 import styles from "@/app/ui/dashboard/products/singleProduct/singleProduct.module.css";
 import Image from "next/image";
 
-const SingleProductPage = () => {
+const SingleProductPage = async ({ params }) => {
+  const { id } = params;
+  const product = await fetchProduct(id);
+
   return (
     <div className={styles.container}>
       <div className={styles.infoContainer}>
         <div className={styles.imgContainer}>
-          <Image src="/noavatar.png" alt="avatar.png" fill />
+          <Image src={product.img || "/noavatar.png"} alt="avatar.png" fill />
         </div>
         Iphone
       </div>
       <div className={styles.formContainer}>
-        <form action="" className={styles.form}>
+        <form action={updateProduct} className={styles.form}>
+          <input type="hidden" name="id" value={product.id} />
           <label>Title</label>
-          <input type="text" name="title" placeholder="John Doe" />
+          <input type="text" name="title" placeholder={product.title} />
           <label>Price</label>
-          <input type="number" name="price" placeholder="499.00" />
+          <input type="number" name="price" placeholder={`$${product.price}`} />
           <label>Stock</label>
-          <input type="number" name="stock" placeholder="49" />
+          <input type="number" name="stock" placeholder={product.stock} />
           <label>Color</label>
-          <input type="text" name="color" placeholder="white" />
+          <input type="text" name="color" placeholder={product.color} />
           <label>Size</label>
-          <input type="text" name="size" placeholder="4'9" />
+          <input type="text" name="size" placeholder={product.size} />
           <label>Category</label>
           <select name="category" id="category">
             <option value="kitchen">Kitchen</option>
@@ -32,10 +38,10 @@ const SingleProductPage = () => {
           </select>
           <label>Description</label>
           <textarea
-            name="description"
-            id="description"
+            name="desc"
+            id="desc"
             rows="16"
-            placeholder="Description..."
+            placeholder={product.desc}
           ></textarea>
           <button>Update</button>
         </form>
--- app/lib/actions.js
+++ app/lib/actions.js
@@ -97,3 +97,26 @@ export const updateUser = async (formData) => {
     revalidatePath("/dashboard/users") //refreshes user table list
     redirect("/dashboard/users") //redirects user to users page
 }
+
+export const updateProduct = async (formData) => {
+    const { id, title, desc, price, stock, color, size, } = Object.fromEntries(formData);
+
+    try {
+        connectToDB();
+
+        const updateFields = {
+            title, desc, price, stock, color, size,
+        }
+
+        Object.keys(updateFields).forEach((key) => (updateFields[key] === "" || undefined) && delete updateFields[key]);
+
+        await Product.findByIdAndUpdate(id, updateFields);
+
+    } catch (error) {
+        console.log(error)
+        throw new Error("Failed to update product!")
+    }
+
+    revalidatePath("/dashboard/products") //refreshes user table list
+    redirect("/dashboard/products") //redirects user to products page
+}
